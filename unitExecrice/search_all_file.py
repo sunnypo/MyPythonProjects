18012d24a1bf5e4cea1ceae0c5a2ca7b6134cf12
sunnypo <siyuan0202@gmail.com>
2022-01-21 23:50:46 -0500

    add  basic log

--- unitExecrice/search_all_file.py
+++ unitExecrice/search_all_file.py
@@ -1,4 +1,8 @@
+from datetime import datetime
 from pathlib import Path
+
+
+ignore_files = ['.DS_Store']
 
 
 def find_all_file(src_dir) -> list:
@@ -16,6 +20,8 @@ def search_all_file(file_type_dic, src_dir) -> list:
     file_list = find_all_file(src_dir)
     file_tbc = []  # file to be changed
     for file in file_list:
+        if file.name in ignore_files:
+            pass
         theory = Path(src_dir, "undefined")
         for key in file_type_dic:
             p = file.suffix.lower()[1:]
@@ -26,8 +32,29 @@ def search_all_file(file_type_dic, src_dir) -> list:
         if not theory == reality:  # 字典找key（文件结尾）(theory)= (reality)parent
             file_name = file.name
             file_tbc.append(file)
-            #file_tbc.append(Path(theory, file_name))
+            move_file(file, Path(theory, file_name))
+
     return file_tbc
+
+
+def record_mv_log(action, target_path):
+    log_file = open(
+        "/Users/Sunny/Documents/myPythonProject/MyPythonProjects/unitExecrice/log.txt", 'a')
+    log_line = (f"{datetime.now()}, {action}, ^{target_path}\n")
+    log_file.write(log_line)
+    log_file.close()
+
+
+def move_file(src_file, dest_path):
+    record_mv_log("_locate file to be sorted_", src_file)
+    src_file.rename(dest_path)
+    record_mv_log("_move the sort file from_", dest_path)
+
+# 每行log可能是这样的：
+# yyyy/mm/dd hh:MM:ss create /Users/hd/tmp/images
+# yyyy/mm/dd hh:MM:ss mv /Users/hd/tmp/a.png /Users/hd/tmp/images/a.png
+
+# yyyy/mm/dd hh:MM:ss 是年月日 时分秒
 
 
 folder_name_dict = {
